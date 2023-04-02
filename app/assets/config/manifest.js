a8c43fd1abedc4656526f3f6bd059ec16d25f309
tanay <tanayray007@gmail.com>
2023-04-03 01:17:06 +0530

    3rd April, 2023

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,3 +2,21 @@
 //= link_directory ../stylesheets .css
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
+// function showImage(input) {
+//     if (input.files && input.files[0]) {
+//       var reader = new FileReader();
+
+//       reader.onload = function (e) {
+//         $('#preview_id')
+//           .attr('src', e.target.result)
+//           .width(150)
+//           .height(200);
+//       };
+
+//       reader.readAsDataURL(input.files[0]);
+//     }
+//   }
+//   $('#image_upload_id').on('change', function() {
+//     showImage(this);
+// })
+
---
+++ app/javascript/custom/index.js
@@ -0,0 +1,3 @@
+import { previewImage } from "custom/custom";
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
+eagerLoadControllersFrom("custom", previewImage)
